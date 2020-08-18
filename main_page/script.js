4485f5eb7d367c146f45301807afd2bd81f9aa85
VictorHenrique317 <victor.henrique5800@gmail.com>
2020-08-18 13:37:17 -0300

    First commit

---
+++ main_page/script.js
@@ -0,0 +1,21 @@
+let terms_div = document.getElementById("terms");
+let youtube_div = document.getElementById("youtube");
+let twitter_div = document.getElementById("twitter");
+
+terms_div.addEventListener("click", onTermsClick);
+youtube_div.addEventListener("click", onYoutubeClick);
+twitter_div.addEventListener("click", onTwitterClick);
+
+function onTermsClick(){
+    console.log("terms clicked");
+}
+
+function onYoutubeClick(){
+    console.log("Youtube clicked")
+    window.open("https:\\www.youtube.com.br");
+}
+
+function onTwitterClick(){
+    console.log("Twitter clicked");
+    window.open("https:\\www.twitter.com");
+}
